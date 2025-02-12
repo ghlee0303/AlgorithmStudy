3331a1b603b61c3c37a9d5e1445d1f0fa4afdec8
ghlee0303 <98991849+ghlee0303@users.noreply.github.com>
2025-02-12 14:11:05 +0900

    back_11279 solve

--- src/back/s_11279.java
+++ src/back/s_11279.java
@@ -6,30 +6,40 @@ import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 // The main method must be in a class named "Main".
 public class s_11279 {
-    static Integer[] heapArray = new Integer[0];
+    static Integer[] heapArray;
+    static int heapSize = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int size = Integer.parseInt(br.readLine());
+        int inputSize = Integer.parseInt(br.readLine());
+        int maxHeapSize = 0;
+        Integer[] inputArray = new Integer[inputSize];
 
-        for (int i = 0; i < size; i++) {
-            Integer inputValue = Integer.parseInt(br.readLine());
+        for (int i = 0; i < inputSize; i++) {
+            int inputValue = Integer.parseInt(br.readLine());
+            inputArray[i] = inputValue;
+
+            if (inputValue != 0) {
+                maxHeapSize++;
+            }
+        }
+
+        heapArray = new Integer[maxHeapSize];
+        for (int i = 0; i < inputSize; i++) {
+            int inputValue = inputArray[i];
 
             if (inputValue != 0) {
                 add(inputValue);
-            } else if (heapArray.length == 0) {
+            } else if (heapSize == 0) {
                 bw.write(0 + "\n");
             } else {
                 bw.write(remove() + "\n");
             }
-
-            System.out.println(inputValue + ": "+ Arrays.toString(heapArray));
         }
 
         bw.flush();
@@ -38,54 +48,48 @@ public class s_11279 {
     }
 
     public static void add(int addValue) {
-        int size = heapArray.length;
-        Integer[] resultArray = Arrays.copyOf(heapArray, size + 1);
-        resultArray[size++] = addValue;
+        heapArray[heapSize++] = addValue;
 
-        if (size <= 1) {
-            heapArray = resultArray;
+        if (heapSize <= 1) {
             return;
         }
 
-        int targetIndex = size - 1;
+        int targetIndex = heapSize - 1;
         while (targetIndex > 0) {
             int parentIndex = (targetIndex - 1) / 2;
 
-            int parentValue = resultArray[parentIndex];
-            int targetValue = resultArray[targetIndex];
+            int parentValue = heapArray[parentIndex];
+            int targetValue = heapArray[targetIndex];
 
             if (parentValue > targetValue) {
                 break;
             }
 
-            swap(resultArray, parentIndex, targetIndex);
+            swap(heapArray, parentIndex, targetIndex);
             targetIndex = parentIndex;
         }
-
-        heapArray = resultArray;
     }
 
     public static int remove() {
-        int size = heapArray.length;
+        int result = heapArray[0];
 
-        if (size == 0) {
+        if (heapSize == 0) {
             return 0;
-        } else if (size == 1) {
-            int result = heapArray[0];
-            heapArray = new Integer[0];
+        } else if (heapSize == 1) {
+            heapArray[0] = null;
+            heapSize = 0;
 
             return result;
         }
 
-        int result = heapArray[0];
-        Integer[] resultArray = shiftArray(heapArray);
+        int resultSize = heapSize - 1;
+        heapArray[0] = heapArray[resultSize];
+        heapArray[resultSize] = null;
 
-        // 자식이 있는지 검증 후 있다면 더 큰 값과 바꿈
         int targetIndex = 0;
-        int resultSize = resultArray.length;
         while (true) {
             int leftChildIndex = targetIndex * 2 + 1;
-            int rightChildIndex = targetIndex * 2 + 2;;
+            int rightChildIndex = targetIndex * 2 + 2;
 
             if (leftChildIndex >= resultSize) {
                 break;
@@ -95,33 +99,21 @@ public class s_11279 {
             if (rightChildIndex >= resultSize) {
                 highestIndex = leftChildIndex;
             } else {
-                highestIndex = (resultArray[leftChildIndex] > resultArray[rightChildIndex]) ? leftChildIndex : rightChildIndex;
+                highestIndex = (heapArray[leftChildIndex] > heapArray[rightChildIndex]) ? leftChildIndex : rightChildIndex;
             }
 
-            if (resultArray[targetIndex] >= resultArray[highestIndex]) {
+            if (heapArray[targetIndex] >= heapArray[highestIndex]) {
                 break;
             }
 
-            swap(resultArray, targetIndex, highestIndex);
+            swap(heapArray, targetIndex, highestIndex);
 
             targetIndex = highestIndex;
         }
 
+        heapSize = resultSize;
+
         return result;
-    }
-
-    public static Integer[] shiftArray(Integer[] arr) {
-        int newSize = arr.length - 1;
-        Integer[] newArr = new Integer[newSize];
-        newArr[0] = arr[newSize]; // 마지막 값을 첫 번째로 이동
-
-        if (newSize == 1) {
-            return newArr;
-        }
-
-        System.arraycopy(arr, 1, newArr, 1, newSize - 1);
-
-        return newArr;
     }
 
     public static void swap(Integer[] arr, int index1, int index2) {
