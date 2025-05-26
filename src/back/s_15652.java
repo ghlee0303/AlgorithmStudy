f7f9c19aabc82da594361ad2eda22a17b0ad6881
ghlee0303 <98991849+ghlee0303@users.noreply.github.com>
2025-05-26 16:17:57 +0900

    백준 15652

---
+++ src/back/s_15652.java
@@ -0,0 +1,45 @@
+package back;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class s_15652 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int n, r;
+    static List<Integer> array = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+
+        solve(1);
+
+        bw.flush();
+    }
+
+    public static void solve(int start) throws IOException {
+        if (array.size() == r) {
+            print();
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            array.add(i);
+            solve(i);
+            array.remove(array.size() - 1);
+        }
+    }
+
+    public static void print() throws IOException {
+        for (int i = 0; i < array.size(); i++) {
+            bw.write(array.get(i) + " ");
+        }
+        bw.write("\n");
+    }
+}
