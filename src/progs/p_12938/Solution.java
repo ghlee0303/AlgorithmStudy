3d6e3b84e900d627d31f6f35b1b32dd8af285f58
ghlee0303 <rjs993530@gmail.com>
2026-07-24 15:37:40 +0900

    프로그래머스 최고의 집합

---
+++ src/progs/p_12938/Solution.java
@@ -0,0 +1,28 @@
+package progs.p_12938;
+
+import java.util.*;
+
+/**
+ * 프로그래머스 최고의 집합
+ */
+public class Solution {
+    public int[] solution(int n, int s) {
+        if (n > s) return new int[]{-1};
+
+        int[] result = new int[n];
+
+        int value = s / n;
+        int remainder = s % n;
+
+        for (int i = 0; i < n; i++) {
+            if (i < remainder) {
+                result[i] = value + 1;
+            } else {
+                result[i] = value;
+            }
+        }
+        Arrays.sort(result);
+
+        return result;
+    }
+}
---
+++ src/progs/p_12938/Test.java
@@ -0,0 +1,14 @@
+package progs.p_12938;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n = 2;
+        int s = 9;
+
+        System.out.println(Arrays.toString(solution.solution(n, s)));
+    }
+}
