c9a5494c95f1238fd8ae56ee73378ddd0e419291
ghlee0303 <rjs993530@gmail.com>
2026-07-24 15:39:54 +0900

    프로그래머스 기본 패턴

---
+++ src/progs/p_default/Solution.java
@@ -0,0 +1,12 @@
+package progs.p_default;
+
+import java.util.*;
+
+/**
+ *
+ */
+
+public class Solution {
+    public void solution() {
+    }
+}
---
+++ src/progs/p_default/Test.java
@@ -0,0 +1,8 @@
+package progs.p_default;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+}
