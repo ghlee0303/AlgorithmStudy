4c49426c7b166802cb305395f217158302528fd4
ghlee0303 <rjs993530@gmail.com>
2026-07-24 17:13:30 +0900

    프로그래머스 점 찍기 해설보고함
    해설보고함

---
+++ src/progs/p_140107/Solution.java
@@ -0,0 +1,21 @@
+package progs.p_140107;
+
+/**
+ * 프로그래머스 점 찍기
+ * 해설보고함
+ */
+
+public class Solution {
+    public long solution(int k, int d) {
+        long count = 0;
+
+        for (double i = 0; i <= d; i += k) {
+            long maxY = (long) Math.sqrt(d * d - i * i);
+
+            long value = maxY / k + 1;
+            count += maxY / k + 1;
+        }
+
+        return count;
+    }
+}
---
+++ src/progs/p_140107/Test.java
@@ -0,0 +1,13 @@
+package progs.p_140107;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int k = 2;
+        int d = 5;
+
+        System.out.println(solution.solution(k, d));
+    }
+
+}
